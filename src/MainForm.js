cba14f6823d0197fb22fc3c0450a39cc61c5942c
Ayan-web <ayanmukherjee619@gmail.com>
2021-09-09 20:02:13 +0530

    completed only few test is left

--- src/MainForm.js
+++ src/MainForm.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./MainForm.css";
 import Shortner from "./shortner.svg";
 import CopyIcon from "./CopySvg.svg";
+import Copied from "./Copied.svg"
 import { useState } from "react";
 
 export default function MainForm() {
   const [text, setText] = useState("");
   const [copy, setCopy] = useState(false);
+  const [copied, setCopied ] = useState(false)
 
   return (
     <>
@@ -16,14 +18,25 @@ export default function MainForm() {
             className="textField"
             type="text"
             value={text}
+            onFocus={(e)=>e.target.select()}
             onChange={(e) => {
               setText(e.target.value);
               setCopy(false);
+              setCopied(false)
             }}
           />
           {copy ? (
-            <CopyIcon className="Svgbutton" />
-          ) : (
+            <CopyIcon 
+            onClick={async ()=>{
+              navigator.clipboard.writeText(text)
+              setCopy(false)
+              setCopied(true)
+            }}
+            className="Svgbutton" />
+          ) :copied ? 
+          (<Copied
+            className="TickSvg"
+          />):(
             <Shortner
               onClick={async () => {
                 const encoded = escape(text);
