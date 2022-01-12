c2a1f1d072267ab82fefce332382459a67c9dd12
Ayan-web <ayanmukherjee619@gmail.com>
2022-01-12 10:48:22 +0530

    add mongodb atlas support

--- server/urldatabase.js
+++ server/urldatabase.js
@@ -13,17 +13,18 @@ require('dotenv').config()
  */
 exports.pushLink = async function (url)
 {
-    const client = new MongoClient(`mongodb://localhost:27017/`,{
+    // const client = new MongoClient(`mongodb://localhost:27017/`,{
+	const client = new MongoClient(process.env.MONGODB_URI,{
         usenewurlparser: true,
         useunifiedtopology: true,
         keepalive:1
     })
 
-    client.connect()
     const db = client.db('test')
     const hash = crypto.createHash('sha256').update(url).digest('hex')
     const uniqueId = nanoid(10)
     try{
+	await client.connect()
         await db.collection('links').insertOne({_id:hash,uniqueId:uniqueId,url:url})
     }
     catch(err){
@@ -46,13 +47,13 @@ exports.pushLink = async function (url)
  */
 exports.get = async function (uniqueId)
 {
-    const client = new MongoClient(`mongodb://localhost:27017/`,{
+    const client = new MongoClient(process.env.MONGODB_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         keepAlive:1
     })
 
-    client.connect()
+    await client.connect()
     const db = client.db('test')
     const link = await db.collection('links').findOne({uniqueId:uniqueId})
     client.close()
